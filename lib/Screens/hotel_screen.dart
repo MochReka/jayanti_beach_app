1d8100350e4a5f8c009b7baeb93e76f7009cb00f
MochReka <mochreka87@gmail.com>
2023-12-05 11:25:18 +0700

    Splash, Welcome, Dashboard & Hotel

--- lib/Screens/hotel_screen.dart
+++ lib/Screens/hotel_screen.dart
@@ -1,153 +1,129 @@
 import 'package:flutter/material.dart';
-import 'package:jayanti_beach_app/constants.dart';
-import 'package:jayanti_beach_app/models/tourism_place.dart';
 
-var informationTextStyle = const TextStyle(fontFamily: 'Oxygen');
+void main() {
+  runApp(HotelScreen());
+}
 
 class HotelScreen extends StatelessWidget {
-  final TourismPlace place;
+  const HotelScreen({Key? key}) : super(key: key);
 
-  const HotelScreen({Key? key, required this.place}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HotelList(),
+    );
+  }
+}
+
+class HotelList extends StatelessWidget {
+  final List<Hotel> hotels = [
+    Hotel(name: 'Hotel A', image: 'images/hotel1.jpg'),
+    Hotel(name: 'Hotel B', image: 'images/hotel2.jpg'),
+    Hotel(name: 'Hotel C', image: 'images/hotel3.jpg'),
+    Hotel(name: 'Hotel D', image: 'images/hotel4.jpg'),
+    // Add more hotels with images as needed
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Stack(
-              children: <Widget>[
-                Image.asset(place.imageAsset),
-                SafeArea(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        CircleAvatar(
-                          backgroundColor: primaryColor,
-                          child: IconButton(
-                            icon: const Icon(
-                              Icons.arrow_back,
-                              color: Colors.white,
-                            ),
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                          ),
-                        ),
-                        const FavoriteButton(),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 16.0),
-              child: Text(
-                place.name,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 30.0,
-                  fontFamily: 'Staatliches',
-                ),
+      appBar: AppBar(
+        title: Text('List Hotel'),
+      ),
+      body: ListView.builder(
+        itemCount: hotels.length,
+        itemBuilder: (context, index) {
+          return HotelCard(hotel: hotels[index]);
+        },
+      ),
+    );
+  }
+}
+
+class HotelCard extends StatelessWidget {
+  final Hotel hotel;
+
+  const HotelCard({Key? key, required this.hotel}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(8.0),
+      // Menyesuaikan padding pada Card
+      // Ubah nilai sesuai kebutuhan
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(0.0), // Tidak ada radius
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.black, // Ubah warna sesuai kebutuhan
+            width: 2.0, // Ubah lebar sesuai kebutuhan
+          ),
+        ),
+        child: ListTile(
+          title: Text(hotel.name),
+          // Menyesuaikan contentPadding pada ListTile
+          // Ubah nilai sesuai kebutuhan
+          contentPadding: EdgeInsets.all(30.0),
+          leading: Container(
+            width: 200.0, // Lebar tetap 200.0, sesuaikan dengan preferensi Anda
+            height: 312.5, // Sesuai dengan rasio aspek 16:9 (200 * 9 / 16)
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(hotel.image),
+                fit: BoxFit.cover, // Menggunakan BoxFit.cover
               ),
             ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      const Icon(Icons.calendar_today),
-                      const SizedBox(height: 8.0),
-                      Text(
-                        place.openDays,
-                        style: informationTextStyle,
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      const Icon(Icons.access_time),
-                      const SizedBox(height: 8.0),
-                      Text(
-                        place.openTime,
-                        style: informationTextStyle,
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      const Icon(Icons.monetization_on),
-                      const SizedBox(height: 8.0),
-                      Text(
-                        place.ticketPrice,
-                        style: informationTextStyle,
-                      ),
-                    ],
-                  ),
-                ],
+          ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) =>
+                    DetailScreen(place: hotel.name, image: hotel.image),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                place.description,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 16.0,
-                  fontFamily: 'Oxygen',
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 150,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: place.imageUrls.map((url) {
-                  return Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: Image.network(url),
-                    ),
-                  );
-                }).toList(),
-              ),
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
   }
 }
 
-class FavoriteButton extends StatefulWidget {
-  const FavoriteButton({Key? key}) : super(key: key);
+class DetailScreen extends StatelessWidget {
+  final String place;
+  final String image;
 
-  @override
-  _FavoriteButtonState createState() => _FavoriteButtonState();
-}
-
-class _FavoriteButtonState extends State<FavoriteButton> {
-  bool isFavorite = false;
+  const DetailScreen({Key? key, required this.place, required this.image})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        isFavorite ? Icons.favorite : Icons.favorite_border,
-        color: secondaryColor,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Details for $place'),
       ),
-      onPressed: () {
-        setState(() {
-          isFavorite = !isFavorite;
-        });
-      },
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            image,
+            width: 500.0,
+            height: 500.0,
+            fit: BoxFit.cover,
+          ),
+          SizedBox(height: 16.0),
+          Text('Details for $place'),
+        ],
+      ),
     );
   }
+}
+
+class Hotel {
+  final String name;
+  final String image;
+
+  Hotel({required this.name, required this.image});
 }
