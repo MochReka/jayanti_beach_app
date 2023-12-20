c9a15a22ecd9e106ac828c74910b5a8f1fb41bc9
MochReka <mochreka87@gmail.com>
2023-12-21 06:57:20 +0700

    Add Data Spot

--- lib/models/spot_data.dart
+++ lib/models/spot_data.dart
@@ -7,12 +7,12 @@ class PhotoSpot {
 }
 
 List<PhotoSpot> photoSpotsData = [
-  PhotoSpot('Pantai Indah', 'assets/images/spot1.jpg',
-      'Pantai eksotis dengan pasir putih yang indah.'),
-  PhotoSpot('Hutan Hijau', 'assets/images/spot2.jpg',
-      'Hutan yang sejuk dengan pepohonan hijau dan segar.'),
-  PhotoSpot('Gunung Megah', 'assets/images/spot3.jpg',
-      'Pemandangan megah dari puncak gunung.'),
+  PhotoSpot('Pesisir Kidul', 'assets/images/spot1.jpg',
+      'Spot Foto Bernuansa Sunset'),
+  PhotoSpot('Pesisir kulon', 'assets/images/spot2.jpg',
+      'Spot Foto Bernuansa Sunrise'),
+  PhotoSpot('Cafetaria', 'assets/images/spot3.jpg',
+      'Pemandangan Indah Didekat Cafetaria'),
   PhotoSpot('Gunung Megah', 'assets/images/spot1.jpg',
       'Pemandangan megah dari puncak gunung.'),
   PhotoSpot('Pantai Indah', 'assets/images/spot1.jpg',
